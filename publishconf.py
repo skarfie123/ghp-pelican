cf891265e8f0ae2c75f7c7c6fa756a91f56ccb98
skarfie123 <rahulpai@rocketmail.com>
2023-03-15 23:55:24 +0000

    url

--- publishconf.py
+++ publishconf.py
@@ -3,11 +3,12 @@
 
 import os
 import sys
+
 sys.path.append(os.curdir)
 from pelicanconf import *
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
-SITEURL = ''
+SITEURL = 'https://skarfie123.github.io/ghp-pelican'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
